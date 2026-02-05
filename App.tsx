eacba40c40222eac3b939c8394c38b3ee375f5e6
nppphutai-binhthuan <npp.phutai@gmail.com>
2026-02-05 14:52:54 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -33,7 +33,7 @@ import {
 } from 'lucide-react';
 import * as XLSX from 'xlsx';
 import { GroupType, ImportItem, BasicUnitMap } from './types';
-import { processImportData } from './services/geminiService';
+import { processImportData } from './geminiService';
 
 // --- Sub-components ---
 
